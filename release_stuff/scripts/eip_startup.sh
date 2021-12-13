9ee9da2c6b97173a473631d95e897a17842ba622
jpboane <jpboane@gmail.com>
2021-12-13 13:52:41 +0200

    spring_artemis_port and spring_artemis_host are now edited directly in application properties file

--- release_stuff/scripts/eip_startup.sh
+++ release_stuff/scripts/eip_startup.sh
@@ -19,5 +19,8 @@ cd $EIP_HOME
 
 source scripts/setenv.sh
 
+sed -i "s/spring_artemis_host/$spring_artemis_host/g" application-sender.properties
+sed -i "s/spring_artemis_port/$spring_artemis_port/g" application-sender.properties
+
 java -jar -Dspring.profiles.active=$EIP_MODE openmrs-eip-app-sender.jar &
 echo -n "APPLICATION STARTED IN BACKGROUND."
