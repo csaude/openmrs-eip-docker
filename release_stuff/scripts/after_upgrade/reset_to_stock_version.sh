1356686ebdd6eb930d8f4a88dd3ce1073a39dbab
jpboane <jpboane@gmail.com>
2021-12-17 16:45:22 +0200

    Monor changes

--- release_stuff/scripts/after_upgrade/reset_to_stock_version.sh
+++ release_stuff/scripts/after_upgrade/reset_to_stock_version.sh
@@ -2,29 +2,21 @@
 
 set timeout -1
 
+HOST_DIR=/home/eip/prg/docker/openmrs-eip-docker/bkps
 HOME_DIR=/home/eip
-LOG_DIR=$HOME_DIR/shared/logs
-OLD_EIP_LOG_DIR=$HOME_DIR/logs
+SHARED_DIR=$HOME_DIR/shared
+LOG_DIR=$SHARED_DIR/logs
 
+if [ ! -d "$LOG_DIR/eip" ]; then
+       mkdir -p $EIP_LOG_DIR
+       echo "THE EIP LOG DIR WAS CREATED" | tee -a $LOG_DIR/reset_to_stock.log
 
-if [ ! -d "$LOG_DIR" ]; then
-       mkdir -p $LOG_DIR
-       echo "THE LOG DIR WAS CREATED" | tee -a $LOG_DIR/reset_to_stock.log
 fi
 
+mv $HOME_DIR/logs* $LOG_DIR/eip/ 
+mv $HOME_DIR/.debezium $SHARED_DIR/
 
-mv $OLD_EIP_LOG_DIR/* $LOG_DIR/eip/ 
-
-
-spawn scp -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -r /home/eip/tmp_scripts eip@localhost:/home/eip/prg/docker/openmrs-eip-docker/
-expect "eip@localhost's password:"
-send -- "#eIP123#\n"
-
-spawn scp -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -r /home/eip/.debezium_ eip@localhost:/home/eip/prg/docker/openmrs-eip-docker/bkps
-expect "eip@localhost's password:"
-send -- "#eIP123#\n"
-
-spawn scp -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -r /home/eip/shared eip@localhost:/home/eip/prg/docker/openmrs-eip-docker/bkps
+spawn scp -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -r $SHARED_DIR eip@localhost:$HOST_DIR/
 expect "eip@localhost's password:"
 send -- "#eIP123#\n"
 
