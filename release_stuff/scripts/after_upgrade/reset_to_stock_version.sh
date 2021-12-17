26e0053a84b144bd5ee0508d4190dff12985e860
jpboane <jpboane@gmail.com>
2021-12-17 17:17:11 +0200

    More modifications

--- release_stuff/scripts/after_upgrade/reset_to_stock_version.sh
+++ release_stuff/scripts/after_upgrade/reset_to_stock_version.sh
@@ -2,7 +2,7 @@
 
 set timeout -1
 
-HOST_DIR=/home/eip/prg/docker/openmrs-eip-docker/bkps
+HOST_DIR=/home/eip/prg/docker/eip-docker-testing
 HOME_DIR=/home/eip
 SHARED_DIR=$HOME_DIR/shared
 LOG_DIR=$SHARED_DIR/logs
@@ -20,7 +20,8 @@ spawn scp -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -r $SHARED
 expect "eip@localhost's password:"
 send -- "#eIP123#\n"
 
-spawn ssh -o StrictHostKeyChecking=no eip@localhost "git -C /home/eip/prg/docker/openmrs-eip-docker/bkps reset --hard; git -C /home/eip/prg/docker/openmrs-eip-docker/bkps pull origin main; docker-compose -f /home/eip/prg/docker/openmrs-eip-docker/docker-compose.yml up --force-recreate -d"
+spawn ssh -o StrictHostKeyChecking=no eip@localhost "git -C $HOST_DIR reset --hard; git -C $HOST_DIR pull origin main; docker-compose -f $HOST_DIR/docker-compose.yml up --force-recreate -d"
+#spawn ssh -o StrictHostKeyChecking=no eip@localhost "git -C /home/eip/prg/docker/openmrs-eip-docker/bkps reset --hard; git -C /home/eip/prg/docker/openmrs-eip-docker/bkps pull origin main; docker-compose -f /home/eip/prg/docker/openmrs-eip-docker/docker-compose.yml up --force-recreate -d"
 expect "eip@localhost's password:"
 send -- "#eIP123#\n"
 expect eof
