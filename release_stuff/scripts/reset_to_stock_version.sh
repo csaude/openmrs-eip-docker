15c43e866890e8e4a5201511b95bf77acf545f8e
JPboane <jpboane@gmail.com>
2021-12-17 14:19:29 +0200

    Update reset_to_stock_version.sh

--- release_stuff/scripts/reset_to_stock_version.sh
+++ release_stuff/scripts/reset_to_stock_version.sh
@@ -1,6 +1,13 @@
 #!/usr/bin/expect -f
 
 set timeout -1
+mkdir /home/eip/tmp_scripts
+cp /home/eip/scripts/* /home/eip/tmp_scripts/
+
+spawn scp -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -r /home/eip/tmp_scripts eip@localhost:/home/eip/prg/docker/openmrs-eip-docker/tmp_scripts
+expect "eip@localhost's password:"
+send -- "#eIP123#\n"
+
 spawn scp -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -r /home/eip/.debezium_ eip@localhost:/home/eip/prg/docker/openmrs-eip-docker/bkps
 expect "eip@localhost's password:"
 send -- "#eIP123#\n"
