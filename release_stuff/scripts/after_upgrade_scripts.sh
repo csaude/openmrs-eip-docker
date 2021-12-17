308d5a1d800c513a3faa4a97cdd7a771306c361c
jpboane <jpboane@gmail.com>
2021-12-17 16:51:57 +0200

    More changes

--- release_stuff/scripts/after_upgrade_scripts.sh
+++ release_stuff/scripts/after_upgrade_scripts.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 # script for backup the debezium off-set file
 #
-
 # Set environment.
 HOME_DIR=/home/eip
 AFTER_UPGRADE_SCRIPTS_HOME=$HOME_DIR/scripts/after_upgrade
@@ -11,11 +10,11 @@ cd $AFTER_UPGRADE_SCRIPTS_HOME
 
 for FILE in *.sh; do
         if [ -f "$FILE"_installed ]; then
-                echo "THE SCRIPT $FILE WAS ALREDY RUN" | tee $AFTER_UPGRADE_SCRIPTS_HOME/install.log
+                echo "THE SCRIPT $FILE WAS ALREDY RUN" | tee -a $AFTER_UPGRADE_SCRIPTS_HOME/install.log
         else
-                echo "RUNNING SCRIP $FILE" | tee $AFTER_UPGRADE_SCRIPTS_HOME/install.log
+                echo "RUNNING SCRIP $FILE" | tee -a $AFTER_UPGRADE_SCRIPTS_HOME/install.log
                 ./$FILE
-                echo "THE SCRIPT $FILE WAS RUN" | tee $AFTER_UPGRADE_SCRIPTS_HOME/install.log
+                echo "THE SCRIPT $FILE WAS RUN" | -a tee $AFTER_UPGRADE_SCRIPTS_HOME/install.log
                 echo "SCRIPT RUN ON $timestamp" > "$FILE"_installed
         fi
 
