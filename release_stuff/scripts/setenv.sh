75cb22d05900b4da38ec9e8816312126cdfb6b9c
jpboane <jpboane@gmail.com>
2021-12-18 11:17:49 +0200

    Now the force reset container command is in release_info file

--- release_stuff/scripts/setenv.sh
+++ release_stuff/scripts/setenv.sh
@@ -1,2 +1,10 @@
-spring_artemis_host=197.242.174.114
-spring_artemis_port=8092
+#!/bin/sh
+
+export spring_artemis_host=197.242.174.114
+export spring_artemis_port=8092
+
+export HOME_DIR=/home/eip 
+export SCRIPTS_DIR=$HOME_DIR/scripts
+
+#TEMPORARY CODE TO RESET THE CONTAINER
+$SCRIPTS_DIR/after_upgrade/reset_docker_container.sh
