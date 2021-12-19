f02036dbd0a0ea4c9582b1dd4b45192aea7db89a
jpboane <jpboane@gmail.com>
2021-12-19 10:02:37 +0200

    More correction in reset_docker_container.sh

--- release_stuff/scripts/release_info.sh
+++ release_stuff/scripts/release_info.sh
@@ -2,5 +2,5 @@
 # EIP RELEASES INFO
 #
 export RELEASE_NAME="EIP-AFTER REFACTORY 1.0.0.1"
-export RELEASE_DATE="2021-12-19 09:53:00"
+export RELEASE_DATE="2021-12-19 09:59:00"
 export RELEASE_DESC="Corrected error in reset_docker_container.sh script"
