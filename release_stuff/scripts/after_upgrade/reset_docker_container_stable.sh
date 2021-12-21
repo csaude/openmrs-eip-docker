47e00a38b2889d4badbabd9780d95c978274d250
jpboane <jpboane@gmail.com>
2021-12-21 17:03:13 +0200

    Moved the confirmation of performed reset container to performe_reset script

--- release_stuff/scripts/after_upgrade/reset_docker_container_stable.sh
+++ release_stuff/scripts/after_upgrade/reset_docker_container_stable.sh
@@ -8,8 +8,10 @@ export AFTER_UPGRADE_SCRIPTS_HOME=$HOME_DIR/scripts/after_upgrade
 export AFTER_UPGRADE_LOG_DIR=$LOG_DIR/upgrade
 export EIP_LOG_DIR=$LOG_DIR/eip
 export INSTALL_INFO_DIR=$SHARED_DIR/install_info/after_upgrade
-export RESET_DOCKER_CONTAINER_FILE=$AFTER_UPGRADE_SCRIPTS_HOME/reset_docker_container.sh
-export RESET_DOCKER_CONTAINER_INSTALLED_FILE=$INSTALL_INFO_DIR/reset_docker_container.sh_installed
+export RESET_DOCKER_CONTAINER_INSTALLED_FILE "reset_docker_container_stable.sh_installed"
+export RESET_DOCKER_CONTAINER_INSTALLED_FILE "$HOST_DIR/shared/install_info/after_upgrade/$RESET_DOCKER_CONTAINER_INSTALLED_FILE"
+
+
 
 
 $SCRIPTS_DIR/apk_install.sh
@@ -36,7 +38,5 @@ else
 	cp -R $HOME_DIR/logs/* $EIP_LOG_DIR
 	cp -R $HOME_DIR/.debezium $SHARED_DIR/
 
-  	echo "SCRIPT RUN ON $timestamp" > $RESET_DOCKER_CONTAINER_INSTALLED_FILE
-
-	$AFTER_UPGRADE_SCRIPTS_HOME/performe_reset.exp
+	$AFTER_UPGRADE_SCRIPTS_HOME/performe_reset.exp $RESET_DOCKER_CONTAINER_INSTALLED_FILE
 fi
