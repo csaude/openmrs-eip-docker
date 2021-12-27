607ff1d997ae13316d87569c02ea7ce8c6c74638
jpboane <jpboane@gmail.com>
2021-12-27 11:13:56 +0200

    Launched release EIP-AFTER REFACTORY 1.0.0.2

--- release_stuff/cron/send_update_log.sh
+++ release_stuff/cron/send_update_log.sh
@@ -1,2 +1,2 @@
-echo "*/4       *       *       *       *       /home/eip/scripts/send_updates_logs.sh" >> /etc/crontabs/root
-#echo "0       */5       *       *       *       /home/eip/scripts/send_udates_logs.sh" >> /etc/crontabs/root
+#echo "*/4       *       *       *       *       /home/eip/scripts/send_updates_logs.sh" >> /etc/crontabs/root
+echo "0       */6       *       *       *       /home/eip/scripts/send_udates_logs.sh" >> /etc/crontabs/root
--- release_stuff/scripts/release_info.sh
+++ release_stuff/scripts/release_info.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 # EIP RELEASES INFO
 #
-export RELEASE_NAME="EIP-AFTER REFACTORY 1.0.0.1"
-export RELEASE_DATE="2021-12-19 09:59:00"
-export RELEASE_DESC="Corrected error in reset_docker_container.sh script"
+export RELEASE_NAME="EIP-AFTER REFACTORY 1.0.0.2"
+export RELEASE_DATE="2021-12-27 11:00:00"
+export RELEASE_DESC="Send Email Script Added + Reset Container Again"
