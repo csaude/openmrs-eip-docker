97755b3ea1b1f9e4ac0ceab96fb7529f0a7e9138
JPboane <jpboane@gmail.com>
2021-12-27 15:42:20 +0200

    Update updates.sh

--- release_stuff/cron/updates.sh
+++ release_stuff/cron/updates.sh
@@ -1,2 +1,2 @@
 #echo "*/3       *       *       *       *       /home/eip/scripts/run_updates.sh" >> /etc/crontabs/root
-echo "0       */5       *       *       *       /home/eip/scripts/run_updates.sh" >> /etc/crontabs/root
+echo "0       */2       *       *       *       /home/eip/scripts/run_updates.sh" >> /etc/crontabs/root
