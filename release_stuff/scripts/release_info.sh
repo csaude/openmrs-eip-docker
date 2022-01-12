09657f85d6cb5f32fe5657c117f7c62d6e8556ea
JP Boane <jpboane@gmail.com>
2022-01-12 12:09:28 +0000

    Updated openmrs-eip to the latest version

--- release_stuff/scripts/release_info.sh
+++ release_stuff/scripts/release_info.sh
@@ -2,5 +2,5 @@
 # EIP RELEASES INFO
 #
 export RELEASE_NAME="EIP release 2.0.0.0"
-export RELEASE_DATE="2022-01-10 11:00:00"
+export RELEASE_DATE="2022-01-12 14:00:00"
 export RELEASE_DESC="Latest version re-installed after centralization server re-installed"
