d07343e0bab08e4da1d1d6f6fcc1b18895badd6b
JP Boane <jpboane@gmail.com>
2022-01-13 14:09:22 +0200

    Downgrade to old EIP jar

--- release_stuff/scripts/release_info.sh
+++ release_stuff/scripts/release_info.sh
@@ -2,5 +2,5 @@
 # EIP RELEASES INFO
 #
 export RELEASE_NAME="EIP release 2.0.0.0"
-export RELEASE_DATE="2022-01-12 14:00:00"
-export RELEASE_DESC="Latest version re-installed after centralization server re-installed"
+export RELEASE_DATE="2022-01-13 14:00:00"
+export RELEASE_DESC="Stable release after new central server installed"
