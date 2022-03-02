1f2fd75daab7cd3996e3c68cad21b5b8e92019d2
JP Boane <jpboane@gmail.com>
2022-03-02 11:24:31 +0200

    Resolved issues on re-sync

--- release_stuff/etc/eptssync/scripts/eptssync_re_sync.sh
+++ release_stuff/etc/eptssync/scripts/eptssync_re_sync.sh
@@ -17,11 +17,13 @@ CONFIG_FILE_ORIGINAL=$EPTSSYNC_HOME/conf/detect_changed_records.json
 cp $CONFIG_FILE_ORIGINAL $CONFIG_FILE
 
 #PREPARE THE CONFIG FILE
-sed -i 's/db_sync_senderId/$db_sync_senderId/g' $CONFIG_FILE
-sed -i 's/openmrs_db_host/$openmrs_db_host/g' $CONFIG_FILE
-sed -i 's/openmrs_db_port/$openmrs_db_port/g' $CONFIG_FILE
-sed -i 's/openmrs_db_name/$openmrs_db_name/g' $CONFIG_FILE
-sed -i 's/spring_openmrs_datasource_password/$spring_openmrs_datasource_password/g' $CONFIG_FILE
+sed -i "s/origin_app_location_code/$origin_app_location_code/g" $CONFIG_FILE
+sed -i "s/openmrs_db_host/$openmrs_db_host/g" $CONFIG_FILE
+sed -i "s/openmrs_db_port/$openmrs_db_port/g" $CONFIG_FILE
+sed -i "s/openmrs_db_name/$openmrs_db_name/g" $CONFIG_FILE
+sed -i "s/spring_openmrs_datasource_password/$spring_openmrs_datasource_password/g" $CONFIG_FILE
+
+mkdir $EPTSSYNC_HOME/logs
 
 # Start application.
 echo -n "Starting EPTS Application"
