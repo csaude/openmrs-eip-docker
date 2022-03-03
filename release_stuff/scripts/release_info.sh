4a68de1aa7e3cb641ac8d6fa5b3a46acef2e28ba
JP Boane <jpboane@gmail.com>
2022-03-03 09:03:39 +0200

    New release after resolved cron job issues

--- release_stuff/scripts/release_info.sh
+++ release_stuff/scripts/release_info.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 # EIP RELEASES INFO
 #
-export RELEASE_NAME="EIP release 2.0.0.0"
-export RELEASE_DATE="2022-01-13 14:45:00"
-export RELEASE_DESC="Stable release after new central server installed"
+export RELEASE_NAME="EIP release 2.0.0.1"
+export RELEASE_DATE="2022-03-03 9:05:00"
+export RELEASE_DESC="Resolved problem with cron jobs"
