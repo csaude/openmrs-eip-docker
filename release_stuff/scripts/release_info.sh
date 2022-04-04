9cba5b9bf7d3f3a96ef27e0a5d08d7de50c915d6
lccpinto <luis.pinto.it@gmail.com>
2022-04-04 15:08:07 +0200

    EC-152 - Moving dbsync releases from github to another storage

--- release_stuff/scripts/release_info.sh
+++ release_stuff/scripts/release_info.sh
@@ -6,4 +6,4 @@ export RELEASE_DATE="2022-03-03 9:05:00"
 export RELEASE_DESC="Resolved problem with cron jobs"
 
 export OPENMRS_EIP_APP_RELEASE_URL="https://github.com/FriendsInGlobalHealth/openmrs-eip-docker/releases/download/v2.0.0.2-beta.1/openmrs-eip-app-1.0-SNAPSHOT.jar"
-export EPRSSYNC_API_RELEASE_URL="https://github.com/FriendsInGlobalHealth/openmrs-eip-docker/releases/download/v2.0.0.2-beta.1/eptssync-api-1.0-SNAPSHOT.jar"
+export EPTSSYNC_API_RELEASE_URL="https://github.com/FriendsInGlobalHealth/openmrs-eip-docker/releases/download/v2.0.0.2-beta.1/eptssync-api-1.0-SNAPSHOT.jar"
