74acf9bce0c24fc11e13aff5ecc02c08e3a850f5
JPboane <jpboane@gmail.com>
2022-09-28 14:39:18 +0200

    Initializing preparation to release v3

--- release_stuff/scripts/release_info.sh
+++ release_stuff/scripts/release_info.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 # EIP RELEASES INFO
 #
-export RELEASE_NAME="EIP release 2.0.0.1"
-export RELEASE_DATE="2022-03-03 9:05:00"
-export RELEASE_DESC="Resolved problem with cron jobs"
+export RELEASE_NAME="EIP release 3.0.0.0"
+export RELEASE_DATE="2022-09-28 12:00:00"
+export RELEASE_DESC="New release with several improvments and new feactures"
 
-export OPENMRS_EIP_APP_RELEASE_URL="https://github.com/FriendsInGlobalHealth/openmrs-eip-docker/releases/download/v2.0.1.0/openmrs-eip-app-1.0-SNAPSHOT.jar"
-export EPTSSYNC_API_RELEASE_URL="https://github.com/FriendsInGlobalHealth/openmrs-eip-docker/releases/download/v2.0.1.0/eptssync-api-1.0-SNAPSHOT.jar"
+export OPENMRS_EIP_APP_RELEASE_URL="https://github.com/FriendsInGlobalHealth/openmrs-eip-docker/releases/download/untagged-4b28085b193f17b1e673/openmrs-eip-app-1.2-SNAPSHOT.jar"
+export EPTSSYNC_API_RELEASE_URL="https://github.com/FriendsInGlobalHealth/openmrs-eip-docker/releases/download/untagged-4b28085b193f17b1e673/eptssync-api-1.0-SNAPSHOT.jar"
