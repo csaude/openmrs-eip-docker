1106f32a9fd0ee4c093fde9189c42428a00342c9
JPboane <jpboane@gmail.com>
2022-09-28 15:54:46 +0200

    Auto update testing 1 - Release date changed

--- release_stuff/scripts/release_info.sh
+++ release_stuff/scripts/release_info.sh
@@ -2,7 +2,7 @@
 # EIP RELEASES INFO
 #
 export RELEASE_NAME="EIP release 3.0.0.0"
-export RELEASE_DATE="2022-09-28 12:00:00"
+export RELEASE_DATE="2022-09-28 16:10:00"
 export RELEASE_DESC="New release with several improvments and new feactures"
 
 export OPENMRS_EIP_APP_RELEASE_URL="https://github.com/FriendsInGlobalHealth/openmrs-eip-docker/releases/download/v3.0.0.0/openmrs-eip-app-1.2-SNAPSHOT.jar"
