2b947685b4b2f8270a6bee9a20f0d61e31cb2c7e
JPboane <jpboane@gmail.com>
2022-12-05 09:00:09 +0200

    Updated release info

--- release_stuff/scripts/release_info.sh
+++ release_stuff/scripts/release_info.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
 # EIP RELEASES INFO
 #
-export RELEASE_NAME="EIP release 3.0.0.0"
-export RELEASE_DATE="2022-09-28 16:10:00"
+
+export RELEASE_NAME="EIP release 2.0.0.1"
+export RELEASE_DATE="2022-03-03 9:05:00"
 export RELEASE_DESC="New release with several improvments and new feactures"
 
-export OPENMRS_EIP_APP_RELEASE_URL="https://github.com/FriendsInGlobalHealth/openmrs-eip-docker/releases/download/v3.0.0.0/openmrs-eip-app-1.0-SNAPSHOT.jar"
+export OPENMRS_EIP_APP_RELEASE_URL="https://github.com/FriendsInGlobalHealth/openmrs-eip-docker/releases/download/v3.0.0.0/openmrs-eip-app-1.0.jar"
 export EPTSSYNC_API_RELEASE_URL="https://github.com/FriendsInGlobalHealth/openmrs-eip-docker/releases/download/v3.0.0.0/eptssync-api-1.0-SNAPSHOT.jar"
