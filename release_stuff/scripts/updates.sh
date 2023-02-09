576268ef92e7911b207c2d2c9bbe1127705f4979
JPboane <jpboane@gmail.com>
2023-02-09 15:21:44 +0200

    Changed upgrade constraint to upgrade specific remote sites

--- release_stuff/scripts/updates.sh
+++ release_stuff/scripts/updates.sh
@@ -15,6 +15,15 @@ SHARED_DIR="$HOME_DIR/shared"
 LOG_DIR="$SHARED_DIR/logs/upgrade"
 RELEASES_PACKAGES_DIR="$SHARED_DIR/releases"
 
+updateAllowedToCurrentSite(){
+
+        for site_name in ${applay_updates_to[@]}; do
+                if [ "$site_name"  = "$db_sync_senderId" ]; then
+                        return 1;
+                fi
+        done
+}
+
 if [ -d "$LOG_DIR" ]; then
        echo "THE LOG DIR EXISTS" #| tee -a $LOG_DIR/upgrade.log
 else
@@ -69,58 +78,67 @@ else
 	echo "REMOTE RELEASE INFO {NAME: $REMOTE_RELEASE_NAME, DATE: $REMOTE_RELEASE_DATE} " #| tee -a $LOG_DIR/upgrade.log
 
 	if [ "$LOCAL_RELEASE_DATE" != "$REMOTE_RELEASE_DATE" ]; then
-		UPDATED=true
+
+		updateAllowedToCurrentSite
+
+		updateAllowed=$?
+
+		if [ "$updateAllowed" = 1 ]; then
+			UPDATED=true
 		
-		echo "UPDATES FOUND..." #| tee -a $LOG_DIR/upgrade.log
-		echo "PERFORMING UPDATE STEPS..." #| tee -a $LOG_DIR/upgrade.log
+			echo "UPDATES FOUND..." #| tee -a $LOG_DIR/upgrade.log
+			echo "PERFORMING UPDATE STEPS..." #| tee -a $LOG_DIR/upgrade.log
 
-		# Downloading release packages
-		echo "Verifying $REMOTE_RELEASE_NAME packages download status"
-		$SCRIPTS_DIR/download_release.sh "$RELEASES_PACKAGES_DIR" "$REMOTE_RELEASE_NAME" "$OPENMRS_EIP_APP_RELEASE_URL" "$EPTSSYNC_API_RELEASE_URL"
+			# Downloading release packages
+			echo "Verifying $REMOTE_RELEASE_NAME packages download status"
+			$SCRIPTS_DIR/download_release.sh "$RELEASES_PACKAGES_DIR" "$REMOTE_RELEASE_NAME" "$OPENMRS_EIP_APP_RELEASE_URL" "$EPTSSYNC_API_RELEASE_URL"
 
-		CURRENT_RELEASES_PACKAGES_DIR="$RELEASES_PACKAGES_DIR/$REMOTE_RELEASE_NAME"
+			CURRENT_RELEASES_PACKAGES_DIR="$RELEASES_PACKAGES_DIR/$REMOTE_RELEASE_NAME"
 
-		RELEASE_PACKAGES_DOWNLOAD_COMPLETED="$CURRENT_RELEASES_PACKAGES_DIR/download_completed"
-		if [ ! -f "$RELEASE_PACKAGES_DOWNLOAD_COMPLETED" ]
-		then
-		   echo "Error trying to download release packages: $REMOTE_RELEASE_NAME. See previous messages."
-		   echo "Update process failed"
-		   rm $ONGOING_UPDATE_INFO_FILE
-		   exit 1
+			RELEASE_PACKAGES_DOWNLOAD_COMPLETED="$CURRENT_RELEASES_PACKAGES_DIR/download_completed"
+			if [ ! -f "$RELEASE_PACKAGES_DOWNLOAD_COMPLETED" ]
+			then
+			   echo "Error trying to download release packages: $REMOTE_RELEASE_NAME. See previous messages."
+			   echo "Update process failed"
+			   rm $ONGOING_UPDATE_INFO_FILE
+			   exit 1
+			fi
+
+			echo "STOPPING EIP APPLICATION.." #| tee -a $LOG_DIR/upgrade.log
+			$SCRIPTS_DIR/eip_stop.sh
+			echo "EIP APLICATION STOPPED!" #| tee -a $LOG_DIR/upgrade.log
+		
+			echo "EIP APLICATION STOPPED!" #| tee -a $LOG_DIR/upgrade.log
+			echo "PERFORMING UPDATES..." #| tee -a $LOG_DIR/upgrade.log
+
+			echo "Removing $HOME_DIR/cron folder"
+			rm -fr $HOME_DIR/cron
+			echo "Removing $HOME_DIR/scripts folder"
+			rm -fr $HOME_DIR/scripts
+			echo "Removing $HOME_DIR/etc folder"
+			rm -fr $HOME_DIR/etc
+			echo "Removing $HOME_DIR/routes folder"
+			rm -fr $HOME_DIR/routes
+
+			echo "Copying recursively from $RELEASE_DIR to $HOME_DIR"
+			cp -R $RELEASE_DIR/* $HOME_DIR/
+			echo "Copying recursively from $EPTSSYNC_SETUP_STUFF_DIR to $EPTSSYNC_HOME_DIR"
+			cp -R $EPTSSYNC_SETUP_STUFF_DIR/* $EPTSSYNC_HOME_DIR
+
+			# copying release packages
+			EIP_PACKAGE_RELEASE_FILE_NAME=$(echo "$OPENMRS_EIP_APP_RELEASE_URL" | rev | cut -d'/' -f 1 | rev)
+			EPTSSYNC_PACKAGE_RELEASE_FILE_NAME=$(echo "$EPTSSYNC_API_RELEASE_URL" | rev | cut -d'/' -f 1 | rev)
+
+			echo "Copying $EIP_PACKAGE_RELEASE_FILE_NAME to $HOME_DIR/openmrs-eip-app-sender.jar"
+			cp "$CURRENT_RELEASES_PACKAGES_DIR/$EIP_PACKAGE_RELEASE_FILE_NAME" "$HOME_DIR/openmrs-eip-app-sender.jar"
+
+			echo "Copying $EPTSSYNC_PACKAGE_RELEASE_FILE_NAME to $EPTSSYNC_HOME_DIR/eptssync-api-1.0-SNAPSHOT.jar"
+			cp "$CURRENT_RELEASES_PACKAGES_DIR/$EPTSSYNC_PACKAGE_RELEASE_FILE_NAME" "$EPTSSYNC_HOME_DIR/eptssync-api-1.0-SNAPSHOT.jar"
+
+			echo "ALL FILES WERE COPIED"
+		else
+			echo "Updates found but not allowed for $db_sync_senderId" 
 		fi
-
-		echo "STOPPING EIP APPLICATION.." #| tee -a $LOG_DIR/upgrade.log
-		$SCRIPTS_DIR/eip_stop.sh
-		echo "EIP APLICATION STOPPED!" #| tee -a $LOG_DIR/upgrade.log
-		
-		echo "EIP APLICATION STOPPED!" #| tee -a $LOG_DIR/upgrade.log
-		echo "PERFORMING UPDATES..." #| tee -a $LOG_DIR/upgrade.log
-
-		echo "Removing $HOME_DIR/cron folder"
-		rm -fr $HOME_DIR/cron
-		echo "Removing $HOME_DIR/scripts folder"
-		rm -fr $HOME_DIR/scripts
-		echo "Removing $HOME_DIR/etc folder"
-		rm -fr $HOME_DIR/etc
-		echo "Removing $HOME_DIR/routes folder"
-		rm -fr $HOME_DIR/routes
-
-		echo "Copying recursively from $RELEASE_DIR to $HOME_DIR"
-		cp -R $RELEASE_DIR/* $HOME_DIR/
-		echo "Copying recursively from $EPTSSYNC_SETUP_STUFF_DIR to $EPTSSYNC_HOME_DIR"
-		cp -R $EPTSSYNC_SETUP_STUFF_DIR/* $EPTSSYNC_HOME_DIR
-
-		# copying release packages
-		EIP_PACKAGE_RELEASE_FILE_NAME=$(echo "$OPENMRS_EIP_APP_RELEASE_URL" | rev | cut -d'/' -f 1 | rev)
-		EPTSSYNC_PACKAGE_RELEASE_FILE_NAME=$(echo "$EPTSSYNC_API_RELEASE_URL" | rev | cut -d'/' -f 1 | rev)
-
-		echo "Copying $EIP_PACKAGE_RELEASE_FILE_NAME to $HOME_DIR/openmrs-eip-app-sender.jar"
-		cp "$CURRENT_RELEASES_PACKAGES_DIR/$EIP_PACKAGE_RELEASE_FILE_NAME" "$HOME_DIR/openmrs-eip-app-sender.jar"
-
-		echo "Copying $EPTSSYNC_PACKAGE_RELEASE_FILE_NAME to $EPTSSYNC_HOME_DIR/eptssync-api-1.0-SNAPSHOT.jar"
-		cp "$CURRENT_RELEASES_PACKAGES_DIR/$EPTSSYNC_PACKAGE_RELEASE_FILE_NAME" "$EPTSSYNC_HOME_DIR/eptssync-api-1.0-SNAPSHOT.jar"
-
-		echo "ALL FILES WERE COPIED"
 	else
         	echo "NO UPDATES FOUND..." #| tee -a $LOG_DIR/upgrade.log
 	fi
