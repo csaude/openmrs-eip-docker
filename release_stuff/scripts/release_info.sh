dfddd5469dfd724cfb2ff6350576c98dd8d5963a
JPboane <jpboane@gmail.com>
2023-02-10 11:58:57 +0200

    Update release_info.sh

--- release_stuff/scripts/release_info.sh
+++ release_stuff/scripts/release_info.sh
@@ -5,6 +5,7 @@ HOME_DIR="/home/eip"
 RELEASE_BASE_DIR="$HOME_DIR/openmrs-eip-docker"
 RELEASE_DIR="$RELEASE_BASE_DIR/release_stuff"
 RELEASE_SCRIPTS_DIR="$RELEASE_DIR/scripts"
+ONGOING_UPDATE_INFO_FILE="$HOME_DIR/ongoing_update_info"
 
 export RELEASE_NAME="EIP release v3.0.1.0"
 export RELEASE_DATE="2023-02-09 14:40:00"
@@ -14,5 +15,7 @@ export OPENMRS_EIP_APP_RELEASE_URL="https://github.com/FriendsInGlobalHealth/ope
 export EPTSSYNC_API_RELEASE_URL="https://github.com/FriendsInGlobalHealth/openmrs-eip-docker/releases/download/v3.0.1.0/eptssync-api-1.0-SNAPSHOT.jar"
 
 cd $RELEASE_SCRIPTS_DIR
+
+rm $ONGOING_UPDATE_INFO_FILE
 
 ./updates.sh 2>&1 | tee -a $LOG_DIR/upgrade.log
