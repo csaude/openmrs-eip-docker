efd4372676a380bc1522cbc39f403307503b2f2b
JPboane <jpboane@gmail.com>
2023-02-13 17:27:03 +0200

    Fix 11

--- release_stuff/scripts/release_info.sh
+++ release_stuff/scripts/release_info.sh
@@ -9,19 +9,19 @@ ONGOING_UPDATE_INFO_FILE="$HOME_DIR/ongoing_update_info"
 TMP_UPDATE_DONE="$HOME_DIR/tmp_update_done_01"
 
 export RELEASE_NAME="EIP release 3.0.1.0"
-export RELEASE_DATE="2023-02-13 11:30:00"
+export RELEASE_DATE="2023-02-13 17:30:00"
 export RELEASE_DESC="New release with several improvments and new feactures"
 
 export OPENMRS_EIP_APP_RELEASE_URL="https://github.com/FriendsInGlobalHealth/openmrs-eip-docker/releases/download/v3.0.1.0/openmrs-eip-app-1.1.jar"
 export EPTSSYNC_API_RELEASE_URL="https://github.com/FriendsInGlobalHealth/openmrs-eip-docker/releases/download/v3.0.1.0/eptssync-api-1.0-SNAPSHOT.jar"
 
-cd $RELEASE_SCRIPTS_DIR
 
-rm $ONGOING_UPDATE_INFO_FILE
 
 if [ -f "$TMP_UPDATE_DONE" ]; then 
 	echo "THE TMP UPDATE HAS ALREADY DONE"
 else
+	rm $ONGOING_UPDATE_INFO_FILE
+	cd $RELEASE_SCRIPTS_DIR
 	touch $TMP_UPDATE_DONE
 	./updates.sh 2>&1 | tee -a $LOG_DIR/upgrade.log
 
