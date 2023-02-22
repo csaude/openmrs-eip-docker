9e0f7cd18607152468ad8275d16b4c6f9312723b
JPboane <jpboane@gmail.com>
2023-02-22 10:55:12 +0200

    Enabled SSL for artemis

--- release_stuff/scripts/release_info.sh
+++ release_stuff/scripts/release_info.sh
@@ -5,26 +5,10 @@ HOME_DIR="/home/eip"
 RELEASE_BASE_DIR="$HOME_DIR/openmrs-eip-docker"
 RELEASE_DIR="$RELEASE_BASE_DIR/release_stuff"
 RELEASE_SCRIPTS_DIR="$RELEASE_DIR/scripts"
-ONGOING_UPDATE_INFO_FILE="$HOME_DIR/ongoing_update_info"
-TMP_UPDATE_DONE="$HOME_DIR/tmp_update_done_01"
 
-export RELEASE_NAME="EIP release 3.0.1.0"
-export RELEASE_DATE="2023-02-14 15:30:00"
-export RELEASE_DESC="New release with several improvments and new feactures"
+export RELEASE_NAME="EIP testing release"
+export RELEASE_DATE="2023-02-22 10:45:00"
+export RELEASE_DESC="This is testing release"
 
-export OPENMRS_EIP_APP_RELEASE_URL="https://github.com/FriendsInGlobalHealth/openmrs-eip-docker/releases/download/v3.0.1.0/openmrs-eip-app-1.1.jar"
-export EPTSSYNC_API_RELEASE_URL="https://github.com/FriendsInGlobalHealth/openmrs-eip-docker/releases/download/v3.0.1.0/eptssync-api-1.0-SNAPSHOT.jar"
-
-
-
-if [ -f "$TMP_UPDATE_DONE" ]; then 
-	echo "THE TMP UPDATE HAS ALREADY DONE"
-else
-	rm $ONGOING_UPDATE_INFO_FILE
-	cd $RELEASE_SCRIPTS_DIR
-	touch $TMP_UPDATE_DONE
-	./updates.sh 2>&1 | tee -a $LOG_DIR/upgrade.log
-
-	$RELEASE_SCRIPTS_DIR/eip_stop.sh
-	sleep 30
-fi
+export OPENMRS_EIP_APP_RELEASE_URL="https://github.com/FriendsInGlobalHealth/openmrs-eip-docker/releases/download/testing/openmrs-eip-app-2.0-SNAPSHOT.jar"
+export EPTSSYNC_API_RELEASE_URL="https://github.com/FriendsInGlobalHealth/openmrs-eip-docker/releases/download/testing/eptssync-api-1.0-SNAPSHOT.jar"
