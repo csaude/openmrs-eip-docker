f89b1bd876a30195e46eacaacaafdeacfe283e16
JPboane <jpboane@gmail.com>
2023-02-22 15:20:54 +0200

    Changed the testing dbsync release

--- release_stuff/scripts/release_info.sh
+++ release_stuff/scripts/release_info.sh
@@ -10,5 +10,5 @@ export RELEASE_NAME="EIP testing release"
 export RELEASE_DATE="2023-02-22 10:45:00"
 export RELEASE_DESC="This is testing release"
 
-export OPENMRS_EIP_APP_RELEASE_URL="https://github.com/FriendsInGlobalHealth/openmrs-eip-docker/releases/download/testing/openmrs-eip-app-2.0-SNAPSHOT.jar"
-export EPTSSYNC_API_RELEASE_URL="https://github.com/FriendsInGlobalHealth/openmrs-eip-docker/releases/download/testing/eptssync-api-1.0-SNAPSHOT.jar"
+export OPENMRS_EIP_APP_RELEASE_URL="https://github.com/FriendsInGlobalHealth/openmrs-eip-docker/releases/download/testing_v1.0/openmrs-eip-app-2.0-SNAPSHOT.jar"
+export EPTSSYNC_API_RELEASE_URL="https://github.com/FriendsInGlobalHealth/openmrs-eip-docker/releases/download/testing_v1.0/eptssync-api-1.0-SNAPSHOT.jar"
