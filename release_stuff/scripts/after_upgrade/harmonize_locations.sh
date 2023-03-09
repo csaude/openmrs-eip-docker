5d7df51a27ec0a448d91741bbf1a56057c937179
JPboane <jpboane@gmail.com>
2023-03-09 09:35:39 +0200

    Revert "Added the automatic location harmonization logic"
    This reverts commit aee177bbabcb1f22fdaff7b0321e851d2ea5f0e9.

--- release_stuff/scripts/after_upgrade/harmonize_locations.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-# description: This shell is intended to performe location harmonization in zambezia after upgrade 
-#
-
-EIP_HOME="/home/eip"
-SCRIPTS_DIR="$EIP_HOME/scripts"
-
-$SCRIPTS_DIR/harmonize_locations.sh
