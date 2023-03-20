86ba8c2f6eb4fc26bf5e4f7839644d5e1118c3e0
JPboane <jpboane@gmail.com>
2023-03-20 16:41:38 +0200

    Notification mail is sent after an liquibase unlock

--- release_stuff/scripts/unlock_dbsync_liquibase.sh
+++ release_stuff/scripts/unlock_dbsync_liquibase.sh
@@ -13,8 +13,11 @@ DB_NAME="openmrs_eip_sender_mgt_${db_sync_senderId}"
 LIQUIBASE_UNLOCK_SCRIPT=$HOME_DIR/etc/sql/unload_dbsync_liquibase.sql
 CHECK_STATUS_SCRIPT=$HOME_DIR/liquibase_check_status.sql
 RESULT_SCRIPT=$HOME_DIR/liquibase_check_status.result
+PATH_TO_ERROR_LOG="$HOME_DIR/tmp_unlock_liquibase"
+EMAIL_CONTENT_FILE="$HOME_DIR/tmp_unlock_liquibase_email_content_file"
 
 . $SCRIPTS_DIR/commons.sh
+. $SCRIPTS_DIR/try_to_load_environment.sh
 
 timestamp=$(getCurrDateTime)
 
@@ -36,5 +39,20 @@ else
 
 	logToScreenAndFile "LIQUIBASE UNLOCKED!" $LOG_FILE
 
-	#TODO: Send email
+	MAIL_SUBJECT="EIP REMOTO - LIQUIBASE LOCK INFO"
+
+	echo "Caros" >> $EMAIL_CONTENT_FILE
+	echo "Servimo-nos deste para informar que o liquibase do dbsync no site $db_sync_senderId encontrava-se LOCKED e foi UNLOCKED com sucesso." >> $EMAIL_CONTENT_FILE
+	echo "" >> $EMAIL_CONTENT_FILE
+	echo "" >> $EMAIL_CONTENT_FILE
+	echo "Enviado automaticamente a partir do servidor $db_sync_senderId." >> $EMAIL_CONTENT_FILE
+
+	$SCRIPTS_DIR/send_notification_to_dbsync_administrators.sh "$MAIL_SUBJECT" $EMAIL_CONTENT_FILE $PATH_TO_ERROR_LOG
+
+	if [ -s $PATH_TO_ERROR_LOG ]; then
+		 logToScreenAndFile  "THE NOTIFICATION EMAIL STATUS COULD NOT SENT YET!" $LOG_FILE
+
+        	$SCRIPTS_DIR/schedule_send_notification_to_dbsync_administrators.sh "$MAIL_SUBJECT" "$EMAIL_CONTENT_FILE"
+	fi
+
 fi
