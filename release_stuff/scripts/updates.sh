0eb89463ebc85457ef72864e62856115a9025f0b
JPboane <jpboane@gmail.com>
2023-03-22 10:04:35 +0200

    Changed the check for ongoing update logic

--- release_stuff/scripts/updates.sh
+++ release_stuff/scripts/updates.sh
@@ -10,7 +10,6 @@ SCRIPTS_DIR="$HOME_DIR/scripts"
 RELEASE_SCRIPTS_DIR="$RELEASE_DIR/scripts"
 EPTSSYNC_SETUP_STUFF_DIR="$RELEASE_DIR/etc/eptssync"
 EPTSSYNC_HOME_DIR="$HOME_DIR/application/eptssync"
-ONGOING_UPDATE_INFO_FILE="$HOME_DIR/ongoing_update_info"
 SHARED_DIR="$HOME_DIR/shared"
 LOG_DIR="$SHARED_DIR/logs/upgrade"
 RELEASES_PACKAGES_DIR="$SHARED_DIR/releases"
@@ -39,144 +38,130 @@ else
 fi
 
 
-if [ -f "$ONGOING_UPDATE_INFO_FILE" ]; then
-	echo "THERE IS ANOTHER UPDATE PROCESS ONGOING" #| tee -a $LOG_DIR/upgrade.log
-else
-	touch $ONGOING_UPDATE_INFO_FILE 
+timestamp=`date +%Y-%m-%d_%H-%M-%S`
 
-	timestamp=`date +%Y-%m-%d_%H-%M-%S`
+echo "CHECKING FOR UPDATES AT $timestamp" #| tee -a $LOG_DIR/upgrade.log
+echo "-------------------------------------------------------------" #| tee -a $LOG_DIR/upgrade.log
 
-	echo "CHECKING FOR UPDATES AT $timestamp" #| tee -a $LOG_DIR/upgrade.log
-	echo "-------------------------------------------------------------" #| tee -a $LOG_DIR/upgrade.log
+git config --global user.email "epts.centralization@fgh.org.mz"
+git config --global user.name "epts.centralization"
 
-	git config --global user.email "epts.centralization@fgh.org.mz"
-	git config --global user.name "epts.centralization"
-
-	#Pull changes from remote project
-	echo "LOOKING FOR EIP PROJECT UPDATES" #| tee -a $LOG_DIR/upgrade.log
+#Pull changes from remote project
+echo "LOOKING FOR EIP PROJECT UPDATES" #| tee -a $LOG_DIR/upgrade.log
 	
-	echo "PULLING EIP PROJECT FROM DOCKER" #| tee -a $LOG_DIR/upgrade.log
+echo "PULLING EIP PROJECT FROM DOCKER" #| tee -a $LOG_DIR/upgrade.log
 	
-	git -C $RELEASE_BASE_DIR clean -df
-	git -C $RELEASE_BASE_DIR reset --hard
-	git -C $RELEASE_BASE_DIR fetch
-	git -C $RELEASE_BASE_DIR pull origin
+git -C $RELEASE_BASE_DIR clean -df
+git -C $RELEASE_BASE_DIR reset --hard
+git -C $RELEASE_BASE_DIR fetch
+git -C $RELEASE_BASE_DIR pull origin
 
-	branch_name=$(getGitBranch $GIT_BRANCHES_DIR)
+branch_name=$(getGitBranch $GIT_BRANCHES_DIR)
 
-	if [ -z $branch_name ]; then
-		echo "The git branch name for site $db_sync_senderId was not found"
-		echo "Aborting upgrade process..."
+if [ -z $branch_name ]; then
+	echo "The git branch name for site $db_sync_senderId was not found"
+	echo "Aborting upgrade process..."
 
-		rm $ONGOING_UPDATE_INFO_FILE
 
-		exit 1
-	fi
+	exit 1
+fi
 
-	echo "Detected branch [$branch_name]"
+echo "Detected branch [$branch_name]"
 
-	git checkout $branch_name
+git checkout $branch_name
 
-	git -C $RELEASE_BASE_DIR pull origin $branch_name
+git -C $RELEASE_BASE_DIR pull origin $branch_name
 	
-	echo "EIP PROJECT PULLED FROM GIT REPOSITORY" #| tee -a $LOG_DIR/upgrade.log
+echo "EIP PROJECT PULLED FROM GIT REPOSITORY" #| tee -a $LOG_DIR/upgrade.log
 
-	. $SCRIPTS_DIR/release_info.sh
+. $SCRIPTS_DIR/release_info.sh
 
-	LOCAL_RELEASE_NAME=$RELEASE_NAME
-	LOCAL_RELEASE_DATE=$RELEASE_DATE
+LOCAL_RELEASE_NAME=$RELEASE_NAME
+LOCAL_RELEASE_DATE=$RELEASE_DATE
 
-	. $RELEASE_SCRIPTS_DIR/release_info.sh
+. $RELEASE_SCRIPTS_DIR/release_info.sh
 
-	REMOTE_RELEASE_NAME=$RELEASE_NAME
-	REMOTE_RELEASE_DATE=$RELEASE_DATE
+REMOTE_RELEASE_NAME=$RELEASE_NAME
+REMOTE_RELEASE_DATE=$RELEASE_DATE
 
-	echo "LOCAL RELEASE INFO {NAME: $LOCAL_RELEASE_NAME, DATE: $LOCAL_RELEASE_DATE} " #| tee -a $LOG_DIR/upgrade.log
-	echo "REMOTE RELEASE INFO {NAME: $REMOTE_RELEASE_NAME, DATE: $REMOTE_RELEASE_DATE} " #| tee -a $LOG_DIR/upgrade.log
+echo "LOCAL RELEASE INFO {NAME: $LOCAL_RELEASE_NAME, DATE: $LOCAL_RELEASE_DATE} " #| tee -a $LOG_DIR/upgrade.log
+echo "REMOTE RELEASE INFO {NAME: $REMOTE_RELEASE_NAME, DATE: $REMOTE_RELEASE_DATE} " #| tee -a $LOG_DIR/upgrade.log
 
-	if [ "$LOCAL_RELEASE_DATE" != "$REMOTE_RELEASE_DATE" ]; then
+if [ "$LOCAL_RELEASE_DATE" != "$REMOTE_RELEASE_DATE" ]; then
 
-		checIfupdateIsAllowedToCurrentSite
+	checIfupdateIsAllowedToCurrentSite
 
-		updateAllowed=$?
+	updateAllowed=$?
 
-		if [ "$updateAllowed" = 1 ]; then
-			UPDATED=true
+	if [ "$updateAllowed" = 1 ]; then
+		UPDATED=true
 		
-			echo "UPDATES FOUND..." #| tee -a $LOG_DIR/upgrade.log
-			echo "PERFORMING UPDATE STEPS..." #| tee -a $LOG_DIR/upgrade.log
+		echo "UPDATES FOUND..." #| tee -a $LOG_DIR/upgrade.log
+		echo "PERFORMING UPDATE STEPS..." #| tee -a $LOG_DIR/upgrade.log
 
-			# Downloading release packages
-			echo "Verifying $REMOTE_RELEASE_NAME packages download status"
-			$RELEASE_SCRIPTS_DIR/download_release.sh "$RELEASES_PACKAGES_DIR" "$REMOTE_RELEASE_NAME" "$OPENMRS_EIP_APP_RELEASE_URL" "$EPTSSYNC_API_RELEASE_URL"
+		# Downloading release packages
+		echo "Verifying $REMOTE_RELEASE_NAME packages download status"
+		$RELEASE_SCRIPTS_DIR/download_release.sh "$RELEASES_PACKAGES_DIR" "$REMOTE_RELEASE_NAME" "$OPENMRS_EIP_APP_RELEASE_URL" "$EPTSSYNC_API_RELEASE_URL"
 
-			CURRENT_RELEASES_PACKAGES_DIR="$RELEASES_PACKAGES_DIR/$REMOTE_RELEASE_NAME"
+		CURRENT_RELEASES_PACKAGES_DIR="$RELEASES_PACKAGES_DIR/$REMOTE_RELEASE_NAME"
 
-			RELEASE_PACKAGES_DOWNLOAD_COMPLETED="$CURRENT_RELEASES_PACKAGES_DIR/download_completed"
-			if [ ! -f "$RELEASE_PACKAGES_DOWNLOAD_COMPLETED" ]
-			then
-			   echo "Error trying to download release packages: $REMOTE_RELEASE_NAME. See previous messages."
-			   echo "Update process failed"
-			   rm $ONGOING_UPDATE_INFO_FILE
-			   exit 1
-			fi
-
-			echo "STOPPING EIP APPLICATION.." #| tee -a $LOG_DIR/upgrade.log
-			$SCRIPTS_DIR/eip_stop.sh
-			echo "EIP APLICATION STOPPED!" #| tee -a $LOG_DIR/upgrade.log
-		
-			echo "EIP APLICATION STOPPED!" #| tee -a $LOG_DIR/upgrade.log
-			echo "PERFORMING UPDATES..." #| tee -a $LOG_DIR/upgrade.log
-
-			echo "Removing $HOME_DIR/cron folder"
-			rm -fr $HOME_DIR/cron
-			echo "Removing $HOME_DIR/scripts folder"
-			rm -fr $HOME_DIR/scripts
-			echo "Removing $HOME_DIR/etc folder"
-			rm -fr $HOME_DIR/etc
-			echo "Removing $HOME_DIR/routes folder"
-			rm -fr $HOME_DIR/routes
-
-			echo "Copying recursively from $RELEASE_DIR to $HOME_DIR"
-			cp -R $RELEASE_DIR/* $HOME_DIR/
-			echo "Copying recursively from $EPTSSYNC_SETUP_STUFF_DIR to $EPTSSYNC_HOME_DIR"
-			cp -R $EPTSSYNC_SETUP_STUFF_DIR/* $EPTSSYNC_HOME_DIR
-
-			# copying release packages
-			EIP_PACKAGE_RELEASE_FILE_NAME=$(echo "$OPENMRS_EIP_APP_RELEASE_URL" | rev | cut -d'/' -f 1 | rev)
-			EPTSSYNC_PACKAGE_RELEASE_FILE_NAME=$(echo "$EPTSSYNC_API_RELEASE_URL" | rev | cut -d'/' -f 1 | rev)
-
-			echo "Copying $EIP_PACKAGE_RELEASE_FILE_NAME to $HOME_DIR/openmrs-eip-app-sender.jar"
-			cp "$CURRENT_RELEASES_PACKAGES_DIR/$EIP_PACKAGE_RELEASE_FILE_NAME" "$HOME_DIR/openmrs-eip-app-sender.jar"
-
-			echo "Copying $EPTSSYNC_PACKAGE_RELEASE_FILE_NAME to $EPTSSYNC_HOME_DIR/eptssync-api-1.0-SNAPSHOT.jar"
-			cp "$CURRENT_RELEASES_PACKAGES_DIR/$EPTSSYNC_PACKAGE_RELEASE_FILE_NAME" "$EPTSSYNC_HOME_DIR/eptssync-api-1.0-SNAPSHOT.jar"
-
-			echo "ALL FILES WERE COPIED"
-		else
-			echo "Updates found but not allowed for $db_sync_senderId" 
+		RELEASE_PACKAGES_DOWNLOAD_COMPLETED="$CURRENT_RELEASES_PACKAGES_DIR/download_completed"
+		if [ ! -f "$RELEASE_PACKAGES_DOWNLOAD_COMPLETED" ]; then
+		   echo "Error trying to download release packages: $REMOTE_RELEASE_NAME. See previous messages."
+		   echo "Update process failed"
+		   exit 1
 		fi
+
+		echo "STOPPING EIP APPLICATION.." #| tee -a $LOG_DIR/upgrade.log
+		$SCRIPTS_DIR/eip_stop.sh
+		echo "EIP APLICATION STOPPED!" #| tee -a $LOG_DIR/upgrade.log
+		
+		echo "EIP APLICATION STOPPED!" #| tee -a $LOG_DIR/upgrade.log
+		echo "PERFORMING UPDATES..." #| tee -a $LOG_DIR/upgrade.log
+
+		echo "Removing $HOME_DIR/cron folder"
+		rm -fr $HOME_DIR/cron
+		echo "Removing $HOME_DIR/scripts folder"
+		rm -fr $HOME_DIR/scripts
+		echo "Removing $HOME_DIR/etc folder"
+		rm -fr $HOME_DIR/etc
+		echo "Removing $HOME_DIR/routes folder"
+		rm -fr $HOME_DIR/routes
+
+		echo "Copying recursively from $RELEASE_DIR to $HOME_DIR"
+		cp -R $RELEASE_DIR/* $HOME_DIR/
+		echo "Copying recursively from $EPTSSYNC_SETUP_STUFF_DIR to $EPTSSYNC_HOME_DIR"
+		cp -R $EPTSSYNC_SETUP_STUFF_DIR/* $EPTSSYNC_HOME_DIR
+
+		# copying release packages
+		EIP_PACKAGE_RELEASE_FILE_NAME=$(echo "$OPENMRS_EIP_APP_RELEASE_URL" | rev | cut -d'/' -f 1 | rev)
+		EPTSSYNC_PACKAGE_RELEASE_FILE_NAME=$(echo "$EPTSSYNC_API_RELEASE_URL" | rev | cut -d'/' -f 1 | rev)
+
+		echo "Copying $EIP_PACKAGE_RELEASE_FILE_NAME to $HOME_DIR/openmrs-eip-app-sender.jar"
+		cp "$CURRENT_RELEASES_PACKAGES_DIR/$EIP_PACKAGE_RELEASE_FILE_NAME" "$HOME_DIR/openmrs-eip-app-sender.jar"
+
+		echo "Copying $EPTSSYNC_PACKAGE_RELEASE_FILE_NAME to $EPTSSYNC_HOME_DIR/eptssync-api-1.0-SNAPSHOT.jar"
+		cp "$CURRENT_RELEASES_PACKAGES_DIR/$EPTSSYNC_PACKAGE_RELEASE_FILE_NAME" "$EPTSSYNC_HOME_DIR/eptssync-api-1.0-SNAPSHOT.jar"
+
+		echo "ALL FILES WERE COPIED"
 	else
-        	echo "NO UPDATES FOUND..." #| tee -a $LOG_DIR/upgrade.log
+		echo "Updates found but not allowed for $db_sync_senderId" 
 	fi
+else
+       	echo "NO UPDATES FOUND..." #| tee -a $LOG_DIR/upgrade.log
+fi
 
-	if [ -f "$ONGOING_UPDATE_INFO_FILE" ]; then
-		rm $ONGOING_UPDATE_INFO_FILE
-	fi
+$SCRIPTS_DIR/schedule_update_notification.sh
 
-	$SCRIPTS_DIR/schedule_update_notification.sh
+if [ "$UPDATED" ]; then
+	echo "PERFORMING AFTER UPDATE STEPS" #| tee -a $LOG_DIR/upgrade.log
 
-	if [ "$UPDATED" ]; then
- 		echo "PERFORMING AFTER UPDATE STEPS" #| tee -a $LOG_DIR/upgrade.log
+	echo "RE-INSTALLING CRONS!" #| tee -a $LOG_DIR/upgrade.log
+        $SCRIPTS_DIR/install_crons.sh
 
-		echo "RE-INSTALLING CRONS!" #| tee -a $LOG_DIR/upgrade.log
-                $SCRIPTS_DIR/install_crons.sh
+       echo "RUNNING STARTUP SCRIPTS!" #| tee -a $LOG_DIR/upgrade.log
+       $SCRIPTS_DIR/after_upgrade_scripts.sh
 
-                echo "RUNNING STARTUP SCRIPTS!" #| tee -a $LOG_DIR/upgrade.log
-                $SCRIPTS_DIR/after_upgrade_scripts.sh
-
-		echo "RESTARTING EIP APPLICATION!" #| tee -a $LOG_DIR/upgrade.log
-                $SCRIPTS_DIR/eip_startup.sh
-
-	fi
+	echo "RESTARTING EIP APPLICATION!" #| tee -a $LOG_DIR/upgrade.log
+        $SCRIPTS_DIR/eip_startup.sh
 fi
