6435a65c6bda819128bb876ba8a10b9ad89d8e9d
JPboane <jpboane@gmail.com>
2023-03-24 08:57:42 +0200

    Fix 2023-03-24 - 02

--- release_stuff/scripts/configure_ssmtp.sh
+++ release_stuff/scripts/configure_ssmtp.sh
@@ -5,23 +5,27 @@
 # Set environment.
 
 EIP_HOME="/home/eip"
-EIP_SCRIPTS_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+#Discovery where this script was called from which is the current script_dir
+SCRIPTS_DIR=$(readlink -f "$0")
+SCRIPTS_DIR=$(dirname $SCRIPTS_DIR)
 
 echo "Configuring smtp..."
 
-echo "EIP_SCRIPTS_DIR: $EIP_SCRIPTS_DIR"
+echo "EIP_SCRIPTS_DIR: $SCRIPTS_DIR"
 
 CUR_DIR=$(pwd)
 
 echo "CUR_DIR: $CUR_DIR"
 
-cd $EIP_SCRIPTS_DIR
+
+#Enter to script dir and the go to the parent folder which is setup stuff dir of eip home dir
+cd $SCRIPTS_DIR
 cd ../
 
 
 ORIGINAL_SSMTP_CONFIG_FILE=$(pwd)
 ORIGINAL_SSMTP_CONFIG_FILE="$ORIGINAL_SSMTP_CONFIG_FILE/ssmtp.conf"
-
 
 
 . $EIP_SCRIPTS_DIR/commons.sh
@@ -40,6 +44,7 @@ sed -i "s/dbsync_notification_email_smtp_user_pass/$dbsync_notification_email_sm
 sed -i "s/dbsync_notification_email_smtp_host_name/$dbsync_notification_email_smtp_host_name/g" $TEMP_SSMTP_CONFIG_FILE
 sed -i "s/dbsync_notification_email_smtp_host_port/$dbsync_notification_email_smtp_host_port/g" $TEMP_SSMTP_CONFIG_FILE
 sed -i "s/db_sync_senderId/$db_sync_senderId/g" $TEMP_SSMTP_CONFIG_FILE
+
 mv $TEMP_SSMTP_CONFIG_FILE $OS_SMTP_CONFIG_FILE
 
 cd $CUR_DIR
