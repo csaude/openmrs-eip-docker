ec57fbafaef592e27bf302bca879b115169c39c2
JPboane <jpboane@gmail.com>
2023-04-13 09:33:41 +0200

    Force the installation to use the correspondent branch

--- install.sh
+++ install.sh
@@ -13,6 +13,7 @@ INSTALL_FINISHED_REPORT_FILE="$HOME_DIR/install_finished_report_file"
 SHARED_DIR="$HOME_DIR/shared"
 RELEASES_PACKAGES_DIR="$SHARED_DIR/releases"
 GIT_BRANCHES_DIR="$EIP_SETUP_STUFF_DIR/git/branches"
+LOG_FILE="$HOME_DIR/install.log"
 
 APK_CMD=$(which apk)
 
@@ -22,25 +23,30 @@ isDockerInstallation
 isDocker=$?
 
 if [ -f "$INSTALL_FINISHED_REPORT_FILE" ]; then
-        echo "INSTALLATION FINISHED"
+        logToScreenAndFile "INSTALLATION FINISHED"
 else
         timestamp=`date +%Y-%m-%d_%H-%M-%S`
 
-        echo "STARTING EIP INSTALLATION PROCESS AT $timespamp"
+        logToScreenAndFile "STARTING EIP INSTALLATION PROCESS AT $timespamp" $LOG_FILE
 
  	branch_name=$(getGitBranch $GIT_BRANCHES_DIR)
 
         if [ -z $branch_name ]; then
-                echo "The git branch name for site $db_sync_senderId was not found"
-                echo "Aborting the installation process..."
+                logToScreenAndFile "The git branch name for site $db_sync_senderId was not found" $LOG_FILE
+                logToScreenAndFile "Aborting the installation process..." $LOG_FILE
 
                 exit 1
 	else
-		echo "Performing installation on site $db_sync_senderId based on branch $branch_name"
+		logToScreenAndFile "Performing installation on site $db_sync_senderId based on branch $branch_name" $LOG_FILE
+
+		git -C $EIP_SETUP_BASE_DIR clean -df
+		git -C $EIP_SETUP_BASE_DIR reset --hard
+		git -C $EIP_SETUP_BASE_DIR checkout $branch_name
+
         fi
         
         if [ ! -z $APK_CMD ]; then
-           echo "INSTALLING DEPENDENCIES USING APK"
+           logToScreenAndFile "INSTALLING DEPENDENCIES USING APK" $LOG_FILE
            $SETUP_SCRIPTS_DIR/apk_install.sh
         fi
 
@@ -48,25 +54,25 @@ else
 
         . $SETUP_SCRIPTS_DIR/release_info.sh
 
-        echo "FOUND RELEASE {NAME: $RELEASE_NAME, DATE: $RELEASE_DATE} "
+        logToScreenAndFile "FOUND RELEASE {NAME: $RELEASE_NAME, DATE: $RELEASE_DATE} " $LOG_FILE
 
-        echo "PERFORMING INSTALLATION STEPS..."
+        logToScreenAndFile "PERFORMING INSTALLATION STEPS..." $LOG_FILE
 
-        echo "COPPING EIP APP FILES"
+        logToScreenAndFile "COPPING EIP APP FILES" $LOG_FILE
 
         cp -R $EIP_SETUP_STUFF_DIR/* $HOME_DIR/
         cp -R $EIP_SETUP_BASE_DIR $HOME_DIR
 
-        echo "CREATING EPTSSYNC HOME DIR"
+        logToScreenAndFile "CREATING EPTSSYNC HOME DIR" $LOG_FILE
         mkdir -p $EPTSSYNC_HOME_DIR
 
-        echo "COPPING EPTSTYC STUFF TO $EPTSSYNC_HOME_DIR"
+        logToScreenAndFile "COPPING EPTSTYC STUFF TO $EPTSSYNC_HOME_DIR" $LOG_FILE
         cp -R $EPTSSYNC_SETUP_STUFF_DIR/* $EPTSSYNC_HOME_DIR
 
 	chmod +x $SCRIPTS_DIR/*.sh
         
         # Downloading release packages
-        echo "Verifying $RELEASE_NAME packages download status"
+        logToScreenAndFile "Verifying $RELEASE_NAME packages download status" $LOG_FILE
         $SCRIPTS_DIR/download_release.sh "$RELEASES_PACKAGES_DIR" "$RELEASE_NAME" "$OPENMRS_EIP_APP_RELEASE_URL" "$EPTSSYNC_API_RELEASE_URL"
         
         CURRENT_RELEASES_PACKAGES_DIR="$RELEASES_PACKAGES_DIR/$RELEASE_NAME"
@@ -74,23 +80,23 @@ else
         RELEASE_PACKAGES_DOWNLOAD_COMPLETED="$CURRENT_RELEASES_PACKAGES_DIR/download_completed"
         if [ ! -f "$RELEASE_PACKAGES_DOWNLOAD_COMPLETED" ]
         then
-           echo "Error trying to download release packages: $RELEASE_NAME. See previous messages."
-           echo "Installation process failed"
+           logToScreenAndFile "Error trying to download release packages: $RELEASE_NAME. See previous messages." $LOG_FILE
+           logToScreenAndFile "Installation process failed" $LOG_FILE
            exit 1
         fi
         
-        EIP_PACKAGE_RELEASE_FILE_NAME=$(echo "$OPENMRS_EIP_APP_RELEASE_URL" | rev | cut -d'/' -f 1 | rev)
-        EPTSSYNC_PACKAGE_RELEASE_FILE_NAME=$(echo "$EPTSSYNC_API_RELEASE_URL" | rev | cut -d'/' -f 1 | rev)
+        EIP_PACKAGE_RELEASE_FILE_NAME=$(logToScreenAndFile "$OPENMRS_EIP_APP_RELEASE_URL" | rev | cut -d'/' -f 1 | rev)
+        EPTSSYNC_PACKAGE_RELEASE_FILE_NAME=$(logToScreenAndFile "$EPTSSYNC_API_RELEASE_URL" | rev | cut -d'/' -f 1 | rev)
 
-        echo "Copying $EIP_PACKAGE_RELEASE_FILE_NAME to $HOME_DIR/openmrs-eip-app-sender.jar"
+        logToScreenAndFile "Copying $EIP_PACKAGE_RELEASE_FILE_NAME to $HOME_DIR/openmrs-eip-app-sender.jar" $LOG_FILE
         cp "$CURRENT_RELEASES_PACKAGES_DIR/$EIP_PACKAGE_RELEASE_FILE_NAME" "$HOME_DIR/openmrs-eip-app-sender.jar"
         
-        echo "Copying $EPTSSYNC_PACKAGE_RELEASE_FILE_NAME to $EPTSSYNC_HOME_DIR/eptssync-api-1.0-SNAPSHOT.jar"
+        logToScreenAndFile "Copying $EPTSSYNC_PACKAGE_RELEASE_FILE_NAME to $EPTSSYNC_HOME_DIR/eptssync-api-1.0-SNAPSHOT.jar" $LOG_FILE
         cp "$CURRENT_RELEASES_PACKAGES_DIR/$EPTSSYNC_PACKAGE_RELEASE_FILE_NAME" "$EPTSSYNC_HOME_DIR/eptssync-api-1.0-SNAPSHOT.jar"
         
-        echo "ALL FILES WERE COPIED"
+        logToScreenAndFile "ALL FILES WERE COPIED" $LOG_FILE
 
-        echo "INSTALLING CRONS"
+        logToScreenAndFile "INSTALLING CRONS" $LOG_FILE
         $SCRIPTS_DIR/install_crons.sh
 
 
@@ -99,7 +105,20 @@ else
 	fi
 
         timestamp=`date +%Y-%m-%d_%H-%M-%S`
-        echo "Installation finished at $timestamp" >> $INSTALL_FINISHED_REPORT_FILE
+        logToScreenAndFile "Installation finished at $timestamp" $INSTALL_FINISHED_REPORT_FILE
+
+	MAIL_SUBJECT="EIP REMOTO - SETUP INFO"
+	EMAIL_CONTENT_FILE="$LOG_FILE"
+        PATH_TO_ERROR_LOG="$HOME_DIR/tmp_error_setup_email_notification"
+
+	$SCRIPTS_DIR/send_notification_to_dbsync_administrators.sh "$MAIL_SUBJECT" $EMAIL_CONTENT_FILE $PATH_TO_ERROR_LOG
+
+        if [ -s $PATH_TO_ERROR_LOG ]; then
+                 logToScreenAndFile  "THE NOTIFICATION EMAIL STATUS COULD NOT SENT YET!" $LOG_FILE
+
+                $SCRIPTS_DIR/schedule_send_notification_to_dbsync_administrators.sh "$MAIL_SUBJECT" "$EMAIL_CONTENT_FILE"
+        fi
+
 fi
 
 if [ $isDocker = 1 ]; then
