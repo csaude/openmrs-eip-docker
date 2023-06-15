81e5dda8c0dd5ce4109f5ff9da199eb6c8cccdc1
JPboane <jpboane@gmail.com>
2023-06-15 16:57:02 +0200

    Fix 2023-06-15 01

--- release_stuff/cron/try_to_schedule_check_for_updates.sh
+++ release_stuff/cron/try_to_schedule_check_for_updates.sh
@@ -1,1 +1,1 @@
-0       *       *       *       *       /home/eip/scripts/try_to_schedule_check_for_updates.sh
+*/5       *       *       *       *       /home/eip/scripts/try_to_schedule_check_for_updates.sh
