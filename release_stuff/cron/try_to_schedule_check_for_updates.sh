99a57227357176a95e36727aa240b310a5ba0764
JPboane <jpboane@gmail.com>
2023-06-20 11:18:28 +0200

    Fixing check for update period for production

--- release_stuff/cron/try_to_schedule_check_for_updates.sh
+++ release_stuff/cron/try_to_schedule_check_for_updates.sh
@@ -1,2 +1,1 @@
-#*/20       *       *       *       *       /home/eip/scripts/try_to_schedule_check_for_updates.sh
-*/5       *       *       *       *       /home/eip/scripts/try_to_schedule_check_for_updates.sh
+*/20       *       *       *       *       /home/eip/scripts/try_to_schedule_check_for_updates.sh
