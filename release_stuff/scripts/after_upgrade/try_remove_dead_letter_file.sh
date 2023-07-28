ab12e17e835f4e1d0da995893a0d9b9deb8190aa
JPboane <jpboane@gmail.com>
2023-07-28 10:28:18 +0200

    Added command to remove RESCHEDULE folder

--- release_stuff/scripts/after_upgrade/try_remove_dead_letter_file.sh
+++ release_stuff/scripts/after_upgrade/try_remove_dead_letter_file.sh
@@ -3,11 +3,25 @@
 HOME_DIR="/home/eip"
 LOG_DIR="$SHARED_DIR/logs/upgrade"
 DEAD_LETTER="/root/dead.letter"
+RESCHEDULE_FOLDER="/home/eip/shared/mail/reschedule"
+
+echo "TRY TO REMOVE dead.letter FILE..." | tee -a $LOG_DIR/upgrade.log
 
 if [ -f "$DEAD_LETTER" ]; then
         echo "THE DEAD LETTER FILE EXISTS... REMOVING IT..." | tee -a $LOG_DIR/upgrade.log
  	rm $DEAD_LETTER
-        echo "DEAD LETTER FILE REMOD!" | tee -a $LOG_DIR/upgrade.log
+        echo "DEAD LETTER FILE REMOVED!" | tee -a $LOG_DIR/upgrade.log
 else
        echo "THE DEAD LETTER FILER DOES NOT EXISTS" | tee -a $LOG_DIR/upgrade.log
 fi
+
+echo "TRY TO REMOVE RESCHEDULE FOLDER..." | tee -a $LOG_DIR/upgrade.log
+
+if [ -d "$RESCHEDULE_FOLDER" ]; then
+        echo "THE RESCHEDULE FOLDER EXISTS... REMOVING IT..." | tee -a $LOG_DIR/upgrade.log
+	rm -fr /home/eip/shared/mail/reschedule
+        echo "RESCHEDULE FOLDER REMOVED!" | tee -a $LOG_DIR/upgrade.log
+else
+       echo "THE RESCHEDULE FOLDER DOES NOT EXISTS" | tee -a $LOG_DIR/upgrade.log
+fi
+
