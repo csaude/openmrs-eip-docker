0e13c302306e8c1056d704a7520b501caa9e0998
daniel <chirindzadaniel@gmail.com>
2023-09-04 16:14:15 +0200

    Fix 2

--- release_stuff/scripts/prod_cabo_delgado_setenv.sh
+++ release_stuff/scripts/prod_cabo_delgado_setenv.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 #export spring_artemis_host=epts-cabo-delgado.fgh.org.mz
-export spring_artemis_host=10.10.2.2
+export spring_artemis_host=epts-zambezia.fgh.org.mz
 export spring_artemis_port=51617
