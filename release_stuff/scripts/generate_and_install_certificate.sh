dbc52f0096b1f534f3d313cc6942de6e373c4997
daniel <chirindzadaniel@gmail.com>
2023-09-04 15:19:09 +0200

    Fix script to generate and install cron

--- release_stuff/scripts/generate_and_install_certificate.sh
+++ release_stuff/scripts/generate_and_install_certificate.sh
@@ -3,8 +3,8 @@
 #
 HOME_DIR="/home/eip"
 SCRIPTS_DIR="$HOME_DIR/scripts"
-EIP_MODE=sender
-GIT_BRANCHES_DIR="$HOME_DIR/git/branches"
+CRONS_DIR="$HOME_DIR/cron"
+GENERATE_AND_INSTALL_CERTIFICATE_CRON="$CRONS_DIR/generate_and_install_certificate.sh"
 PATH_TO_CERTIFICATE="$HOME_DIR/artemis.cert"
 
 URL="$spring_artemis_host:$spring_artemis_port"
@@ -12,8 +12,9 @@ $SCRIPTS_DIR/generate_certificate.sh $URL $PATH_TO_CERTIFICATE
 
 if [ ! -s $PATH_TO_CERTIFICATE ]; then
     echo "The certificate cannot be generated now... scheduling..."
-    $SCRIPTS_DIR/try_to_schedule_genarate_and_install_certificate.sh
-    
+    echo "*/5       *       *       *       *       $SCRIPTS_DIR/generate_and_install_certificate.sh" > ${GENERATE_AND_INSTALL_CERTIFICATE_CRON}
+
+    $SCRIPTS_DIR/install_crons.sh 
 else
     if [ -z $JAVA_HOME ];then
         echo "JAVA_HOME is not defined! Configuring it"
@@ -24,4 +25,13 @@ else
     fi
     echo "Using JAVA_HOME =$JAVA_HOME"
     $SCRIPTS_DIR/install_certificate_to_jdk_carcets.sh $PATH_TO_CERTIFICATE "artemis"
+
+
+    if [-f $GENERATE_AND_INSTALL_CERTIFICATE_CRON]; then
+
+        echo "Removing old ceritificate installation cron"
+        rm $GENERATE_AND_INSTALL_CERTIFICATE_CRON
+        $SCRIPTS_DIR/install_crons.sh
+
+    fi
 fi
--- release_stuff/scripts/try_to_schedule_genarate_and_install_certificate.sh
+++
@@ -1,1 +0,0 @@
-*/5       *       *       *       *       /home/eip/scripts/generate_and_install_certificate.sh
