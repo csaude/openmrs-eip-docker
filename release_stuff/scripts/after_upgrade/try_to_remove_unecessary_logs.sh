17af4f30fa59d03fb7f1c858a0ad62baac9cd42a
JPboane <jpboane@gmail.com>
2023-09-22 09:49:57 +0200

    Fix 2023-09-22 03

--- release_stuff/scripts/after_upgrade/try_to_remove_unecessary_logs.sh
+++ release_stuff/scripts/after_upgrade/try_to_remove_unecessary_logs.sh
@@ -5,10 +5,12 @@ HOME_DIR="/home/eip"
 SCRIPTS_DIR="$HOME_DIR/scripts"
 LOG_DIR="$HOME_DIR/shared/logs"
 BKPS_HOME=$HOME_DIR/shared/bkps
+DEAD_LETTER="/root/dead.letter"
+RESCHEDULE_FOLDER="/home/eip/shared/mail/reschedule"
 
 . $SCRIPTS_DIR/commons.sh
 
-for FILE in "$LOG_DIR" "$BKPS_HOME" 
+for FILE in "$LOG_DIR" "$BKPS_HOME" "$DEAD_LETTER" "$RESCHEDULE_FOLDER" 
 do
 	if [ -d "$FILE" ]; then
 		echo "Removing folder $FILE"
