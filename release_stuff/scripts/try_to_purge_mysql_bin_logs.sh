4db8e1a496137a7d9bfb6cd4ba2d5f173059f4fd
JPboane <jpboane@gmail.com>
2023-09-26 17:28:23 +0200

    Fix 2023-09-26 02

--- release_stuff/scripts/try_to_purge_mysql_bin_logs.sh
+++ release_stuff/scripts/try_to_purge_mysql_bin_logs.sh
@@ -17,6 +17,9 @@ LAST_PURGE_REPORT="$BIN_LOGS_DIR/last_purge_report"
 PURGE=0
 PURGE_PERIOD=5
 
+
+. $HOME_DIR/scripts/common.sh
+
 if [ ! -f "$LAST_PURGE_REPORT" ]; then
         PURGE=1
         logToScreenAndFile "Last purge report was not found! The purge will be done now..." $NOTIFICATIONS_LOG
@@ -35,9 +38,16 @@ if [ ! -d "$BIN_LOGS_DIR" ]; then
         mkdir $BIN_LOGS_DIR
 fi
 
-#Split the binlog data using (") and take the 16th position wich is binlog name
-binlog_name=$(echo "$data" | cut -d '"' -f 16)
 
-echo "PURGE BINARY LOGS TO '$binlog_name';" > $DB_SCRIPT
+if [ $PURGE -eq 1 ]; then
+	#Split the binlog data using (") and take the 16th position wich is binlog name
+	binlog_name=$(echo "$data" | cut -d '"' -f 16)
 
-$HOME_DIR/scripts/execute_script_on_db.sh $DB_HOST $DB_HOST_PORT $DB_USER $DB_PASSWD $DB_NAME $DB_SCRIPT $RESULT_SCRIPT
+	echo "PURGE BINARY LOGS TO '$binlog_name';" > $DB_SCRIPT
+
+	$HOME_DIR/scripts/execute_script_on_db.sh $DB_HOST $DB_HOST_PORT $DB_USER $DB_PASSWD $DB_NAME $DB_SCRIPT $RESULT_SCRIPT
+
+	currDateTime=$(getCurrDateTime)
+
+	echo "Last purge at $currDateTime" > $LAST_PURGE_REPORT
+fi
