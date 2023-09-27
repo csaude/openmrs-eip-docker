f9f190347b88883e00d7dcaebd5acbf9df44c56a
JPboane <jpboane@gmail.com>
2023-09-27 12:10:25 +0200

    Dbsync Deployment project V6 released

--- release_stuff/cron/try_to_purge_mysql_bin_logs.sh
+++ release_stuff/cron/try_to_purge_mysql_bin_logs.sh
@@ -1,2 +1,1 @@
-#15       */3       *       *       *       /home/eip/scripts/try_to_purge_mysql_bin_logs.sh
-*/2       *       *       *       *       /home/eip/scripts/try_to_purge_mysql_bin_logs.sh
+15       */3       *       *       *       /home/eip/scripts/try_to_purge_mysql_bin_logs.sh
