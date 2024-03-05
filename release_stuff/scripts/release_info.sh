0c309188d88bcf8e2fc440d7dc8d719c5a758775
Daniel Chirinda <chirindzadaniel@gmail.com>
2024-03-05 19:22:58 +0200

    Update release info

--- release_stuff/scripts/release_info.sh
+++ release_stuff/scripts/release_info.sh
@@ -9,7 +9,7 @@ RELEASE_SCRIPTS_DIR="$RELEASE_DIR/scripts"
 
 export RELEASE_NAME="EIP-Release-8.0.0"
 export RELEASE_DATE="2024-03-20 20:00:00"
-export RELEASE_DESC="DBSYnc v1.5x compatibility and Centralization Features"
+export RELEASE_DESC="DBSYnc v1.6x compatibility and Centralization Features"
 
 export OPENMRS_EIP_APP_RELEASE_URL="https://github.com/csaude/openmrs-eip-docker/releases/download/V8.0.0/openmrs-eip-app-1.6.0.jar"
 export EPTS_ETL_API_RELEASE_URL="https://github.com/csaude/openmrs-eip-docker/releases/download/V8.0.0/epts-etl-api-1.0.jar"
