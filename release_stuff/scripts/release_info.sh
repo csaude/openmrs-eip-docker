4594070fc8b823c6d65e163b131bb77b28d5c6b3
Daniel Chirinda <chirindzadaniel@gmail.com>
2024-06-27 14:09:49 +0200

    HotFix for bug trying to syncronize deleted entity

--- release_stuff/scripts/release_info.sh
+++ release_stuff/scripts/release_info.sh
@@ -11,6 +11,6 @@ export RELEASE_NAME="EIP-Release-8.0.0"
 export RELEASE_DATE="2024-03-20 20:00:00"
 export RELEASE_DESC="DBSYnc v1.6x upgrade of Java version and Centralization Features"
 
-export OPENMRS_EIP_APP_RELEASE_URL="https://github.com/csaude/openmrs-eip-docker/releases/download/V8.0.0/openmrs-eip-app-1.6.0.jar"
+export OPENMRS_EIP_APP_RELEASE_URL="https://github.com/csaude/openmrs-eip-docker/releases/download/V8.0.0/openmrs-eip-app-1.6.4.jar"
 export EPTS_ETL_API_RELEASE_URL="https://github.com/csaude/openmrs-eip-docker/releases/download/V8.0.0/epts-etl-api-1.0.jar"
 export CENTRALIZATION_FEATURES_MANAGER_RELEASE_URL="https://github.com/csaude/openmrs-eip-docker/releases/download/V8.0.0/centralization-features-manager-1.0.jar"
