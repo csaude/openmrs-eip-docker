0eb14ea6eaa38987678662d66d6f8d5a99b3915e
JPboane <jpboane@gmail.com>
2024-12-19 12:11:51 +0200

    Change artemis Port

--- release_stuff/scripts/prod_inhambane_setenv.sh
+++ release_stuff/scripts/prod_inhambane_setenv.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 export spring_artemis_host=artemis-ib.csaude.org.mz
-export spring_artemis_port=51617
+export spring_artemis_port=40404
 export artemis_ssl_enabled=true
--- release_stuff/scripts/prod_sofala_setenv.sh
+++ release_stuff/scripts/prod_sofala_setenv.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 export spring_artemis_host=artemis-sf.csaude.org.mz
-export spring_artemis_port=51617
+export spring_artemis_port=40405
 export artemis_ssl_enabled=true
