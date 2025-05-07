1cc98660f93227c9409b046bc6a4e222156f0e94
JPboane <jpboane@gmail.com>
2025-05-07 16:32:07 +0200

    Resolve conflict

--- release_stuff/scripts/apt_install.sh
+++ release_stuff/scripts/apt_install.sh
@@ -70,6 +70,11 @@ if [ -z $MYSQL_CLIENT ];then
 	DEBIAN_FRONTEND=noninteractive dpkg -i mysql-apt-config_0.8.29-1_all.deb
 
 	apt update
+        wget https://dev.mysql.com/get/mysql-apt-config_0.8.29-1_all.deb
+
+        DEBIAN_FRONTEND=noninteractive dpkg -i mysql-apt-config_0.8.29-1_all.deb
+
+        apt update
 
 	echo "INSTALLING MYSQL CLIENT" | tee -a $LOG_DIR/apt_install.log
 	apt install -y mysql-client
