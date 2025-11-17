aa4c611b06372f839ef121c0ecb39f2e8bfe5bc4
Daniel Chirinda <chirindzadaniel@gmail.com>
2025-11-17 17:56:39 +0200

    Add mariadb client  package

--- release_stuff/scripts/apt_install.sh
+++ release_stuff/scripts/apt_install.sh
@@ -60,29 +60,8 @@ if [ ! -f "$PACKAGE_INSTALLED" ];then
 	MYSQL_CLIENT=$(which mysql)
 
 	if [ -z $MYSQL_CLIENT ];then
-		apt install -y lsb-release
-		apt install -y gnupg
 		apt update
-		wget https://dev.mysql.com/get/mysql-apt-config_0.8.29-1_all.deb
-
-    	    	DEBIAN_FRONTEND=noninteractive dpkg -i mysql-apt-config_0.8.29-1_all.deb
-
-     	   	apt update
-
-		wget https://dev.mysql.com/get/mysql-apt-config_0.8.29-1_all.deb
-
-		DEBIAN_FRONTEND=noninteractive dpkg -i mysql-apt-config_0.8.29-1_all.deb
-
-		apt update
-       		wget https://dev.mysql.com/get/mysql-apt-config_0.8.29-1_all.deb
-
-       		DEBIAN_FRONTEND=noninteractive dpkg -i mysql-apt-config_0.8.29-1_all.deb
-
-       		apt update
-
-		echo "INSTALLING MYSQL CLIENT" | tee -a $LOG_DIR/apt_install.log
-		apt install -y mysql-client
-		echo "MYSQL CLIENT INSTALLED" | tee -a $LOG_DIR/apt_install.log
+		apt install -y mariadb-client		
 	
 	else
 		echo "MYSQL CLIENT ALREADY INSTALLED" | tee -a $LOG_DIR/apt_install.log
